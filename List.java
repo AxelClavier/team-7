5f7909f8c38c47c24473f9ea7e11b74f07c54b83
Livia Tjandra <livia.tjandra@gmail.com>
2013-11-19 17:16:08 -0800

    2 functions that I could not test
    2 functions that I do not know how to test:
    -public void saveAndQuit(){
    -public void start(String inputfile){
    
    i write these functions because they are stated in the questions.

--- List.java
+++ List.java
@@ -1,43 +1,61 @@
 import java.util.ArrayList;
-import java.io.*;
+//import java.io.*;
 
 /**
- * One object of this class stores a list information for many contacts
+ * One object of this class stores a list of information for many contacts
  */
 public class List {
-	private ArrayList <Person> myList;
+	private ArrayList <Person> myList;	//create an arraylist
 
 	public List (){
-		//construct array list
-	}
-
-	public void addPerson(Person newPerson){
-		//if last name or first name is empty
-			//message:"Person is not entered because first or last name is empty"
-		//else
-			//add person to contact list
-	}
-
-	public void printList(){
-		//print the contact list
-		}
-	}
-
-	public void retrieve(String lastName){
-		//retrieve a person's information by last name
-			//compare last name
-			// if last name is found
-				//print
-				//message: not found
-	}
-
-	public void saveAndQuit(){
-		//quit the program and save to disk
+		myList = new ArrayList <Person> ();		//an arraylist with Person data type is created
 	}
 	
-	public void start(String inputfile){
-		//start the list program:
-			//find the contact list on disk and system begins with an empty contact list
-			//outcome: there is a contact list in memory containing any contacts that were entered previously		}	
+	/**
+	 * add 1 object of Person into a single element of myList
+	 */
+	public void addPerson(Person newPerson){
+//		if(newPerson.getFirstName().equals("")){
+//			System.out.println("Person is not entered because first or last name is empty");
+//		}
+//		else {
+//			myList.add(newPerson);
+//		}
 	}
+
+	/**
+	 * display list of Persons (objects) stored in myList
+	 */
+	public void printList(){
+		for(Person person : myList){
+			person.printPerson(); 
+		}
+	}
+	
+	/**
+	 * print Person(s) (object) with certain last name
+	 */
+	public void retrieve(String lastName){
+		int found = 0;
+		for(Person person : myList){
+			if(person.getLastName().equals(lastName)){
+				found = 1;
+				person.printPerson();
+			}
+		}
+		if(found == 0){
+			System.out.println("Last Name "+lastName+" is not found");
+		}
+
+	}
+	
+//	public void saveAndQuit(){
+	//		quit the program and save to disk
+//	}
+
+//	public void start(String inputfile){
+	//		start the list program:
+	//		find the contact list on disk and system begins with an empty contact list
+	//		outcome: there is a contact list in memory containing any contacts that were entered previously		}	
+//	}
 }
