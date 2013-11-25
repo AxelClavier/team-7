8ddae859dab70e8db28566d4cf2638a47d6d15bb
Livia Tjandra <livia.tjandra@gmail.com>
2013-11-25 00:23:25 -0800

    Update Main.java

--- Skeleton/Main.java
+++ Skeleton/Main.java
@@ -1,39 +1,36 @@
+import java.util.Scanner;
+
 /**
  * Contact list program that allows the user to add people to the list,
- * visualize the contacts he/she already inputed, search through the contacts
- * by last name, and save his/her contact list to the hard drive.
+ * visualize the contacts he/she already inputed, search through the contacts by
+ * last name, and save his/her contact list to the hard drive.
  * 
  * AC
  */
 public class Main {
-	public static void main (String args[]) {
+	public static void main(String args[]) {
+		List run = new List();
 		int runVariable = 0;
-		Ancillary run;
-		run = new Ancillary ();
-	    
-	    run.startUp();
-	    
-	    while (runVariable == 0){
-	    	// Prompts the user to choose between four options:
-	    	  // 1. Add new person
-	    	  // 2. Visualize contact list
-	    	  // 3. Search a specific person on the contact list by last name
-	    	  // 4. Exit
-	    	
-	    	// if 1. is chosen:
-	    	  run.addPerson();
-	    	  
-	    	// if 2. is chosen:
-	    	  run.printList();
-	    	  
-	    	// if 3. is chosen:
-	    	  run.search();
-	    	  
-	    	// if 4. is chosen:
-	    	  run.exit();
-	    	  runVariable = 1;
-             }
 
-	    }
+		run.startUp();
+
+		while (runVariable == 0) {
+			int userChoice = run.mainMenu();
+
+			if (userChoice == 1) 
+				run.addPerson();
+			else if (userChoice == 2)
+				run.printList();
+			else if (userChoice == 3)
+				run.search();
+			else if (userChoice == 4) {
+				run.exit();
+				runVariable = 1;
+			} 
+			else
+				System.out.println("Invalid input.");
+		}
 
 	}
+
+}
