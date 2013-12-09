d71eb332e2a1d2ce092a5b1cb7e140453682efc4
Livia Tjandra <livia.tjandra@gmail.com>
2013-12-09 11:42:44 -0800

    Update Person.java

--- Person.java
+++ Person.java
@@ -21,91 +21,91 @@ public class Person implements Comparable <Person> {
 	}
 	
 	/**
-	 * Returns the value of firstName.
+	 * L: Returns the value of firstName.
 	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
 	/**
-	 * Sets the value for firstName to "firstName".
+	 * L: Sets the value for firstName to "firstName".
 	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
 	/**
-	 * Returns the value of lastName.
+	 * L: Returns the value of lastName.
 	 */
 	public String getLastName() {
 		return lastName;
 	}
 
 	/**
-	 * Sets the value for lastName to "lastName".
+	 * L: Sets the value for lastName to "lastName".
 	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
 	/**
-	 * Returns the value of streetAddress.
+	 * L: Returns the value of streetAddress.
 	 */
 	public String getStreetAddress() {
 		return StreetAddress;
 	}
 
 	/**
-	 * Sets the value for streetAddress to "streestAddress".
+	 * L: Sets the value for streetAddress to "streestAddress".
 	 */
 	public void setStreetAddress(String StreetAddress) {
 		this.StreetAddress = StreetAddress;
 	}
 
 	/**
-	 * Returns the value of emailAddress.
+	 * L: Returns the value of emailAddress.
 	 */
 	public String getEmailAddress() {
 		return emailAddress;
 	}
 
 	/**
-	 * Sets the value for emailAddress to "emailAddress".
+	 * L: Sets the value for emailAddress to "emailAddress".
 	 */
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
 
 	/**
-	 * Returns the value of phoneNumber.
+	 * L: Returns the value of phoneNumber.
 	 */
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
 	/**
-	 * Sets the value for phoneNumber to "phoneNumber".
+	 * L: Sets the value for phoneNumber to "phoneNumber".
 	 */
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
 	/**
-	 * Returns the value of notes.
+	 * L: Returns the value of notes.
 	 */
 	public String getNotes() {
 		return notes;
 	}
 
 	/**
-	 * Sets the value for notes to "notes".
+	 * L: Sets the value for notes to "notes".
 	 */
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
 	
 	/**
-	 * Returns a String containing firstName, lastName, StreetAddress, emailAddress, phoneNumber, notes
+	 * L: Returns a String containing firstName, lastName, StreetAddress, emailAddress, phoneNumber, notes
 	 */
 	public String toString() {
 		return "  " + firstName + " " + lastName + "\n    Street Address: " + StreetAddress + "\n    Email address: " + emailAddress
